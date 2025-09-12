46f26d7467b8e0475299ea98efe4ee2ff12b8f06
aurorafreir <aurorafreir@gmail.com>
2025-09-12 14:42:45 +0100

    Updating Brooke scene for parity with Metahuman scene

--- brooke.py
+++ brooke.py
@@ -6,16 +6,15 @@ This is an example of a rig created for a Digitigrade Anthrophormorphic characte
 
 # SYSTEM IMPORTS
 from importlib import reload
+import time
 
 # STANDARD LIBRARY IMPORTS
 import pymel.core as pm
-from pymel.core import nt
 
 # LOCAL APPLICATION IMPORTS
 from rig_on_skeleton import rig_on_skeleton as ros
 
 reload(ros)
-
 
 # EXAMPLE CODE
 """
@@ -27,15 +26,23 @@ brooke.run()
 
 
 def run():
+    print_errors = True
 
-    # FIRST SETUP #
+    # -- FIRST SETUP --
     rig = ros.Rig()
     rig.main_grp = "brooke"
     rig.ensure_setup_is_correct()
 
     driver = "DRIVER"
 
+    pm.refresh()
+
+    # -- CONTROLLER SETUP --
     # ROOT CONTROLS #
+    root_limb = ros.Limb()
+    root_limb.limb_name = "root"
+    root_limb.rig_parent = rig.rig_setup_grp
+    root_limb.ctl_parent = rig.ctls_grp
     global_ctl = ros.CtrlSet(
         ctl_name="global",
         ctl_shape="square_with_point",
@@ -64,7 +71,18 @@ def run():
     )
     root_ctl.create_ctl()
 
+    # add ctls to root_limb.ctl attribute, and append root_limb to rig.limbs
+    root_limb.ctls.extend([global_ctl, global_off_ctl, root_ctl])
+    rig.limbs.append(root_limb)
+
+    pm.refresh()
+
     # HIP CONTROLS #
+    hip_limb = ros.Limb()
+    hip_limb.limb_name = "hip"
+    hip_limb.rig_parent = rig.rig_setup_grp
+    hip_limb.ctl_parent = rig.ctls_grp
+
     hip_ctl = ros.CtrlSet(
         ctl_name="hip",
         ctl_shape="box",
@@ -82,11 +100,17 @@ def run():
         worldSpace=True,
     )
 
-    # constraints #
-    pm.parentConstraint(root_ctl.ctl, "root_drv", maintainOffset=True)
-    pm.parentConstraint(hip_ctl.ctl, "pelvis_drv", maintainOffset=True)
+    # add ctls to hip_limb.ctl attribute, and append hip_limb to rig.limbs
+    hip_limb.ctls.extend([hip_ctl])
+    rig.limbs.append(hip_limb)
+
+    pm.refresh()
 
     # NECK AND HEAD
+    neck_and_head_limb = ros.Limb()
+    neck_and_head_limb.limb_name = "neck_and_head"
+    neck_and_head_limb.rig_parent = rig.rig_setup_grp
+    neck_and_head_limb.ctl_parent = rig.ctls_grp
     neck_01 = ros.CtrlSet(
         ctl_name="neck_01",
         ctl_shape="box",
@@ -136,17 +160,17 @@ def run():
         worldSpace=True,
     )
 
-    # NECK AND HEAD FINALISING
-    pm.parentConstraint(hip_ctl.ctl, neck_01.main_grp, maintainOffset=True)
-    pm.parentConstraint(neck_01.ctl, "neck_01_drv")
-    pm.parentConstraint(neck_02.ctl, "neck_02_drv")
-    pm.parentConstraint(head.ctl, "head_drv")
+    # add ctls to neck_and_head_limb.ctl attribute, and append neck_and_head_limb to rig.limbs
+    neck_and_head_limb.ctls.extend([neck_01, neck_02, head])
+    rig.limbs.append(neck_and_head_limb)
 
-    ros.lock_hide_default_attrs(neck_01.ctl, rotate=False)
-    ros.lock_hide_default_attrs(neck_02.ctl, rotate=False)
-    ros.lock_hide_default_attrs(head.ctl, rotate=False)
+    pm.refresh()
 
     # SHOULDERS
+    shoulder_l = ros.Limb()
+    shoulder_l.limb_name = "shoulder_l"
+    shoulder_l.rig_parent = rig.rig_setup_grp
+    shoulder_l.ctl_parent = rig.ctls_grp
     scap_l = ros.CtrlSet(
         ctl_name="shoulder_l",
         ctl_shape="box",
@@ -164,6 +188,16 @@ def run():
         t=pm.xform("clavicle_l_drv", t=True, query=True, worldSpace=True),
         worldSpace=True,
     )
+
+    shoulder_l.ctls.append(scap_l)
+    rig.limbs.append(shoulder_l)
+
+    pm.refresh()
+
+    shoulder_r = ros.Limb()
+    shoulder_r.limb_name = "shoulder_r"
+    shoulder_r.rig_parent = rig.rig_setup_grp
+    shoulder_r.ctl_parent = rig.ctls_grp
     scap_r = ros.CtrlSet(
         ctl_name="shoulder_r",
         ctl_shape="box",
@@ -183,14 +217,19 @@ def run():
     )
     scap_r.do_mirror()
 
-    # SHOULDERS FINALIZING
-    pm.parentConstraint(scap_l.ctl, "clavicle_l_drv", maintainOffset=True)
-    pm.parentConstraint(scap_r.ctl, "clavicle_r_drv", maintainOffset=True)
+    shoulder_r.ctls.append(scap_r)
+    rig.limbs.append(shoulder_r)
 
-    ros.lock_hide_default_attrs(scap_l.ctl, rotate=False)
-    ros.lock_hide_default_attrs(scap_r.ctl, rotate=False)
+    pm.refresh()
 
     # L HAND SETUP
+    pv_l_main_grp, _, pv_l_placer = ros.place_temp_pv_locators(
+        name="l_arm",
+        upper_joint=pm.PyNode("upperarm_l_drv"),
+        middle_joint=pm.PyNode("lowerarm_l_drv"),
+        lower_joint=pm.PyNode("hand_l_drv"),
+    )
+
     arm_l = ros.ThreeBoneLimb()
     arm_l.limb_name = "arm_l"
     arm_l.input_joints = ["upperarm_l_drv", "lowerarm_l_drv", "hand_l_drv"]
@@ -199,6 +238,7 @@ def run():
     arm_l.rig_parent = rig.rig_setup_grp
     arm_l.ctl_parent = rig.ctls_grp
     arm_l.rig_upper_obj = scap_l.ctl
+    arm_l.verbose = print_errors
     arm_l.create_limb_setup()
     # CONTROLS #
     # driver
@@ -243,11 +283,12 @@ def run():
         shape_size=3,
         parent=arm_l.rig_ctls_grp,
         colour=ros.left_col,
+        mirror=True,
     )
     hand_l_pv_ctl.create_ctl()
     pm.xform(
         hand_l_pv_ctl.main_grp,
-        matrix=pm.xform("l_arm_position_loc", matrix=True, query=True, worldSpace=True),
+        matrix=pm.xform(pv_l_placer, matrix=True, query=True, worldSpace=True),
         worldSpace=True,
     )
     # upperarm_l_fk
@@ -306,15 +347,55 @@ def run():
     arm_l.driver_ctl = hand_l_drv_ctl.ctl
     arm_l.create_three_bone_limb()
 
-    pm.parentConstraint(arm_l.skin_joints[2], hand_l_drv_ctl.main_grp)
+    # # l arm twist
+    # ros.delete_if_exists("upperarm_twist_01_l_orientConstraint1_drv")
+    # ros.delete_if_exists("upperarm_twist_02_l_orientConstraint1_drv")
+    # ros.delete_if_exists("upperarm_correctiveRoot_l_drv_parentConstraint1")
+    #
+    # upperarm_twist_01_oc = pm.orientConstraint(
+    #     arm_l.noroll_upper_joint,
+    #     arm_l.pole_pin_lower_jnt,
+    #     "upperarm_twist_01_l_drv",
+    #     skip=["y", "z"],
+    #     maintainOffset=True,
+    # )
+    # upperarm_twist_01_oc.interpType.set(2)
+    # upperarm_twist_01_oc.attr(f"{arm_l.noroll_upper_joint}W0").set(0.8)
+    # upperarm_twist_01_oc.attr(f"{arm_l.pole_pin_lower_jnt}W1").set(0.2)
+    # upperarm_twist_02_oc = pm.orientConstraint(
+    #     arm_l.noroll_upper_joint,
+    #     arm_l.pole_pin_lower_jnt,
+    #     "upperarm_twist_02_l_drv",
+    #     skip=["y", "z"],
+    #     maintainOffset=True,
+    # )
+    # upperarm_twist_02_oc.interpType.set(2)
+    # upperarm_twist_02_oc.attr(f"{arm_l.noroll_upper_joint}W0").set(0.4)
+    # upperarm_twist_02_oc.attr(f"{arm_l.pole_pin_lower_jnt}W1").set(0.6)
 
-    pm.parentConstraint(arm_l.pole_pin_upper_jnt, "upperarm_l_drv")
-    pm.parentConstraint(arm_l.pole_pin_lower_jnt, "lowerarm_l_drv")
-    pm.parentConstraint(arm_l.skin_joints[2], "hand_l_drv")
+    # add ctls to arm_l.ctl attribute, and append arm_l to rig.limbs
+    arm_l.ctls.extend(
+        [
+            hand_l_drv_ctl,
+            hand_l_ik_ctl,
+            hand_l_pv_ctl,
+            upperarm_l_fk_ctl,
+            lowerarm_l_fk_ctl,
+            hand_l_fk_ctl,
+        ]
+    )
+    rig.limbs.append(arm_l)
 
-    pm.parentConstraint(scap_l.ctl, upperarm_l_fk_ctl.main_grp, maintainOffset=True)
+    pm.refresh()
 
     # R HAND SETUP
+    pv_r_main_grp, _, pv_r_placer = ros.place_temp_pv_locators(
+        name="r_arm",
+        upper_joint=pm.PyNode("upperarm_l_drv"),
+        middle_joint=pm.PyNode("lowerarm_l_drv"),
+        lower_joint=pm.PyNode("hand_l_drv"),
+    )
+
     arm_r = ros.ThreeBoneLimb()
     arm_r.limb_name = "arm_r"
     arm_r.input_joints = ["upperarm_r_drv", "lowerarm_r_drv", "hand_r_drv"]
@@ -323,6 +404,7 @@ def run():
     arm_r.rig_parent = rig.rig_setup_grp
     arm_r.ctl_parent = rig.ctls_grp
     arm_r.rig_upper_obj = scap_r.ctl
+    arm_r.verbose = print_errors
     arm_r.create_limb_setup()
     # CONTROLS #
     # driver
@@ -373,7 +455,7 @@ def run():
     hand_r_pv_ctl.create_ctl()
     pm.xform(
         hand_r_pv_ctl.main_grp,
-        matrix=pm.xform("l_arm_position_loc", matrix=True, query=True, worldSpace=True),
+        matrix=pm.xform(pv_r_placer, matrix=True, query=True, worldSpace=True),
         worldSpace=True,
     )
     # upperarm_r_fk
@@ -438,16 +520,104 @@ def run():
     arm_r.mirror = True
     arm_r.create_three_bone_limb()
 
+    # # r arm twist
+    # ros.delete_if_exists("upperarm_twist_01_r_orientConstraint1_drv")
+    # ros.delete_if_exists("upperarm_twist_02_r_orientConstraint1_drv")
+    # ros.delete_if_exists("upperarm_correctiveRoot_r_drv_parentConstraint1")
+    #
+    # upperarm_twist_01_oc = pm.orientConstraint(
+    #     arm_r.noroll_upper_joint,
+    #     arm_r.pole_pin_lower_jnt,
+    #     "upperarm_twist_01_r_drv",
+    #     skip=["y", "z"],
+    #     maintainOffset=True,
+    # )
+    # upperarm_twist_01_oc.interpType.set(2)
+    # upperarm_twist_01_oc.attr(f"{arm_r.noroll_upper_joint}W0").set(0.8)
+    # upperarm_twist_01_oc.attr(f"{arm_r.pole_pin_lower_jnt}W1").set(0.2)
+    # upperarm_twist_02_oc = pm.orientConstraint(
+    #     arm_r.noroll_upper_joint,
+    #     arm_r.pole_pin_lower_jnt,
+    #     "upperarm_twist_02_r_drv",
+    #     skip=["y", "z"],
+    #     maintainOffset=True,
+    # )
+    # upperarm_twist_02_oc.interpType.set(2)
+    # upperarm_twist_02_oc.attr(f"{arm_r.noroll_upper_joint}W0").set(0.4)
+    # upperarm_twist_02_oc.attr(f"{arm_r.pole_pin_lower_jnt}W1").set(0.6)
+
+    # add ctls to arm_r.ctl attribute, and append arm_r to rig.limbs
+    arm_r.ctls.extend(
+        [
+            hand_r_drv_ctl,
+            hand_r_ik_ctl,
+            hand_r_pv_ctl,
+            upperarm_r_fk_ctl,
+            lowerarm_r_fk_ctl,
+            hand_r_fk_ctl,
+        ]
+    )
+    rig.limbs.append(arm_r)
+
+    # -- CONTROLLER SETUP CONSTRAINTS --
+    # HIPS
+
+    # NECK AND HEAD
+    pm.parentConstraint(hip_ctl.ctl, neck_01.main_grp, maintainOffset=True)
+
+    # SHOULDERS
+    pm.parentConstraint(hip_ctl.ctl, scap_l.main_grp, maintainOffset=True)
+    pm.parentConstraint(hip_ctl.ctl, scap_r.main_grp, maintainOffset=True)
+
+    # ARMS
+    pm.parentConstraint(scap_l.ctl, upperarm_l_fk_ctl.main_grp, maintainOffset=True)
+    pm.parentConstraint(scap_r.ctl, upperarm_r_fk_ctl.main_grp, maintainOffset=True)
+    pm.parentConstraint(arm_l.skin_joints[2], hand_l_drv_ctl.main_grp)
     pm.parentConstraint(arm_r.skin_joints[2], hand_r_drv_ctl.main_grp)
 
+    # -- CONTROLLER TO SKELETON CONSTRAINTS --
+    # HIPS
+    pm.parentConstraint(root_ctl.ctl, "root_drv", maintainOffset=True)
+    pm.parentConstraint(hip_ctl.ctl, "pelvis_drv", maintainOffset=True)
+    # NECK AND HEAD
+    pm.parentConstraint(neck_01.ctl, "neck_01_drv")
+    pm.parentConstraint(neck_02.ctl, "neck_02_drv")
+    pm.parentConstraint(head.ctl, "head_drv")
+    # SHOULDERS
+    pm.parentConstraint(scap_l.ctl, "clavicle_l_drv", maintainOffset=True)
+    pm.parentConstraint(scap_r.ctl, "clavicle_r_drv", maintainOffset=True)
+
+    # pm.orientConstraint(arm_l.dup_parent_joint, "upperarm_correctiveRoot_l_drv", maintainOffset=True)
+    # shoulder_l_twist_half_orientconstraint = pm.orientConstraint(arm_l.pole_pin_upper_jnt, arm_l.dup_upperarm_joint,
+    #                                                              "upperarm_correctiveRootCor_l_drv",
+    #                                                              maintainOffset=True)
+    # shoulder_l_twist_half_orientconstraint.interpType.set(2)
+    # pm.orientConstraint(arm_r.dup_parent_joint, "upperarm_correctiveRoot_r_drv", maintainOffset=True)
+    # shoulder_r_twist_half_orientconstraint = pm.orientConstraint(arm_r.pole_pin_upper_jnt, arm_r.dup_upperarm_joint,
+    #                                                              "upperarm_correctiveRootCor_r_drv",
+    #                                                              maintainOffset=True)
+    # shoulder_r_twist_half_orientconstraint.interpType.set(2)
+
+    # ARMS
+    pm.parentConstraint(arm_l.pole_pin_upper_jnt, "upperarm_l_drv")
+    pm.parentConstraint(arm_l.pole_pin_lower_jnt, "lowerarm_l_drv")
+    pm.parentConstraint(arm_l.skin_joints[2], "hand_l_drv")
     pm.parentConstraint(arm_r.pole_pin_upper_jnt, "upperarm_r_drv")
     pm.parentConstraint(arm_r.pole_pin_lower_jnt, "lowerarm_r_drv")
     pm.parentConstraint(arm_r.skin_joints[2], "hand_r_drv")
 
-    pm.parentConstraint(scap_r.ctl, upperarm_r_fk_ctl.main_grp, maintainOffset=True)
+    # -- ATTRIBUTE FINALISING/LOCKING/HIDING --
+    # HIPS
 
-    # Arms attribute locking
-    for i in [
+    # NECK AND HEAD
+    ros.lock_hide_default_attrs(neck_01.ctl, rotate=False)
+    ros.lock_hide_default_attrs(neck_02.ctl, rotate=False)
+    ros.lock_hide_default_attrs(head.ctl, rotate=False)
+    # SHOULDERS
+    ros.lock_hide_default_attrs(scap_l.ctl, rotate=False)
+    ros.lock_hide_default_attrs(scap_r.ctl, rotate=False)
+    # ARMS
+    for control in [
         upperarm_l_fk_ctl.ctl,
         lowerarm_l_fk_ctl.ctl,
         hand_l_fk_ctl.ctl,
@@ -455,13 +625,23 @@ def run():
         lowerarm_r_fk_ctl.ctl,
         hand_r_fk_ctl.ctl,
     ]:
-        ros.lock_hide_default_attrs(i, rotate=False)
-    for i in [hand_l_pv_ctl.ctl, hand_r_pv_ctl.ctl]:
-        ros.lock_hide_default_attrs(i, rotate=False, translate=False)
-    for i in [hand_l_ik_ctl.ctl, hand_r_ik_ctl.ctl]:
-        ros.lock_hide_default_attrs(i, rotate=False, translate=False)
-    for i in [hand_l_drv_ctl.ctl, hand_r_drv_ctl.ctl]:
-        ros.lock_hide_default_attrs(i)
+        ros.lock_hide_default_attrs(control, rotate=False)  # FK CONTROLS
+    for control in [hand_l_pv_ctl.ctl, hand_r_pv_ctl.ctl]:  # PVs
+        ros.lock_hide_default_attrs(control, rotate=False, translate=False)
+    for control in [hand_l_ik_ctl.ctl, hand_r_ik_ctl.ctl]:  # IK HAND
+        ros.lock_hide_default_attrs(control, rotate=False, translate=False)
+    for control in [hand_l_drv_ctl.ctl, hand_r_drv_ctl.ctl]:  # HAND DRIVER
+        ros.lock_hide_default_attrs(control)
+    # RIG
+    # rig.rig_setup_grp.visibility.set(0)
+    rig.finalise()
 
-    pm.parentConstraint(hip_ctl.ctl, scap_l.main_grp, maintainOffset=True)
-    pm.parentConstraint(hip_ctl.ctl, scap_r.main_grp, maintainOffset=True)
+    # FINALISING
+    pm.delete(pv_l_main_grp, pv_r_main_grp)
+
+    print(f"{time.perf_counter():.2f}: Finished building '{rig.main_grp}'")
+
+    print(f"limbs: {[i.limb_name for i in rig.limbs]}")
+    print(f"limb ctls: {[[y.ctl_name for y in i.ctls] for i in rig.limbs]}")
+
+    return rig
